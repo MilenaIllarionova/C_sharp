6cf3f455b1763d282765725c326c19b2f4a374e4
milena222-a <mlnillarionova@gmail.com>
2023-10-08 17:26:02 +0700

    дз задание 3

--- 001/Program.cs
+++ 001/Program.cs
@@ -1,5 +1,5 @@
 ﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
-using System;
+/*using System;
 
 namespace SecondDigit
 {
@@ -15,14 +15,53 @@ namespace SecondDigit
             Console.WriteLine($"Вторая цифра числа {number} равна {secondDigit}");
         }
     }
-}
-/*Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
-645 -> 5
-78 -> третьей цифры нет
-32679 -> 6
+}*/
+//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+/*using System;
 
+namespace ThirdNum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите трехзначное число: ");
+            int num = int.Parse(Console.ReadLine());
+
+            int thirdNum = (num / 100) % 10;
+
+            if (thirdNum == 0)
+            {
+                Console.WriteLine("В числе нет третьей цифры");
+            }
+            else
+            {
+                Console.WriteLine($"Третья цифра числа {num} равна {thirdNum}");
+            }
+        }
+    }
+}*/
 //Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
-6 -> да
-7 -> да
-1 -> нет
-*/
+using System;
+
+namespace Week
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите день недели: ");
+            int day = int.Parse(Console.ReadLine());
+
+           if (day ==6 || day == 7)
+            {
+                Console.WriteLine("Да");
+            }
+            else
+            {
+                Console.WriteLine("Нет");
+            }
+        }
+    }
+}
+
